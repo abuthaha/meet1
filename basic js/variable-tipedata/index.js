d10ef7586efeb5c1e383f69fb365c17a8d5421c8
abuthaha <aziz.abuthaha@gmail.com>
2022-04-16 15:25:22 +0700

    variable & tipe data

---
+++ basic js/variable-tipedata/index.js
@@ -0,0 +1,46 @@
+/**
+ * VARIABLES
+ * sebuah penampung yang memiliki tipe / jenis data
+ * 
+*/ 
+
+// TIPE DATA / JENIS DATA
+/**
+ * 1. Number
+ * 2. String
+ * 3. Boolean
+ * 4. Object
+ * 5. Undefined
+ * 6. Null
+ */ 
+
+var angka = 10;
+var kata = "halo guys";
+var isAvailable = true;
+
+// console.log(typeof angka);
+// console.log(typeof kata);
+// console.log(typeof isAvailable);
+
+// OPERATOR
+/**
+ * 1. OPERATOR MATH
+ * 2. OPERATOR LOGIC
+ */ 
+
+// Modulus : sisa bagi
+// 7 % 2 = 1
+// 7 % 3 = 1
+// 12 % 7 = 5
+
+// var x = 10;
+// var y = '5';
+
+// var z = x / y;
+// console.log(z);
+
+var a = 15;
+var b = '15';
+
+var c = a === b;
+console.log(c);
