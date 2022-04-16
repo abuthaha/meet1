e9e73a31e1ab842c72d4807ee31552d83cc52919
abuthaha <aziz.abuthaha@gmail.com>
2022-04-16 14:29:29 +0700

    algoritma & pseudocode

---
+++ algoritma/index.js
@@ -0,0 +1,44 @@
+// ALGORITMA
+// 1. Buat variable alas
+// 2. Buat variable tinggi
+// 3. Buat variable luas
+// 4. Isi variable dengan rumus 1/2 kali alas kali tinggi
+// 5. Cetak variable luas
+// 
+// PSEUDOCODE
+// Store alas WITH ANY NUMBER
+// Store tigggi WITH ANY NUMBER
+// 
+// SET luas WITH 1/2 MULTIPLY alas MULTIPLY tinggi
+// DISPLAY luas
+
+// var alas = 8;
+// var tinggi = 5;
+
+// var luas = 1/2 * alas * tinggi;
+// console.log(luas);
+
+/**
+ * ALGORITMA
+ * 1. Buat variable r
+ * 2. Buat variable pi
+ * 3. Isi variable pi dengan 3.14
+ * 4. Buat variable luas
+ * 5. Isi variable luas dengan rumus pi kali r kali r
+ * 
+ * PSEUDOCODE
+ * STORE r WITH ANY NUMBER
+ * STORE pi WITH 3.14
+ * 
+ * SET luas WITH pi MULTIPLY r MULTIPLY r
+ * DISPLAY luas
+ */
+
+var r = 7;
+var pi = 3.14;
+
+var luas = pi * r * r;
+console.log(luas);
+
+var keliling = 2 * pi * r;
+console.log(keliling);
